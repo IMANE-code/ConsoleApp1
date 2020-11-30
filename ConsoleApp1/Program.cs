d9477a5caaf2b98474e0a318191f3e6cd795db3f
IMANE-code <57986793+IMANE-code@users.noreply.github.com>
2020-11-30 15:46:17 +0100

    Ajoutez des fichiers projet.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)//point d'entre
+        {
+            boucle();
+
+
+        }
+        static public void produit()
+        {
+            
+            Console.WriteLine("Entrer le nom de produit");
+            string produit = Console.ReadLine();
+
+            Console.WriteLine("Entrer le prix de produit");
+            string prixTTC = Console.ReadLine();
+
+            Console.WriteLine("Entrer le TVA");
+            string TVA = Console.ReadLine();
+
+            double prixTVA = double.Parse( TVA)/100 * double.Parse(prixTTC);
+
+            Console.WriteLine( "le prix de "+ produit +" TTC est de "+prixTVA+" DH");
+        }
+
+        static public void boucle()
+        {
+            int mult;    
+            Console.WriteLine("entrer la valeur de b :");
+            string b = Console.ReadLine();
+            if (int.Parse(b) >= 2 && int.Parse(b) <= 100)
+            {
+                Console.WriteLine("La multiplication de deux valeur est:");
+                for (int a = 1; a <= 30; a++)
+                {
+                    mult = a * int.Parse(b);
+                    Console.WriteLine(mult);
+                }
+            }else Console.WriteLine(" valeur de b superieur de 100 ou moin à 2 ");
+            }
+        }
+
+    static public void ex2()
+    {
+
+    }
+
+ }
+
